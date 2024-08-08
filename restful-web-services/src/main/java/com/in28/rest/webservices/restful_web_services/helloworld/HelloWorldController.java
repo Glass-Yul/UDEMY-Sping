8fc58b6f339e393e0b6aad676dc39d5cb88f86c7
Glass <dbfl3742@naver.com>
2024-08-08 22:48:03 +0900

    rest api @PathVariable

--- restful-web-services/src/main/java/com/in28/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/in28/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -1,9 +1,6 @@
 package com.in28.rest.webservices.restful_web_services.helloworld;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 // REST API
 @RestController
@@ -15,4 +12,17 @@ public class HelloWorldController {
     public String helloWorld() {
         return "Hello World";
     }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+
+    // Path Parameters
+    // /user/{id}/todos/{id}
+    @GetMapping(path = "hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
 }
